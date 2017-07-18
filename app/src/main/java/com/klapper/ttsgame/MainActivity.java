05aac32149f9e21509bcbb0537fcf2661c2f79b6
KuanLin <1103304154@gm.kuas.edu.tw>
2017-07-18 15:00:04 +0800

    2017.07.18 new version

--- app/src/main/java/com/klapper/ttsgame/MainActivity.java
+++ app/src/main/java/com/klapper/ttsgame/MainActivity.java
@@ -1,6 +1,13 @@
 package com.klapper.ttsgame;
 
+import android.Manifest;
+import android.content.Context;
+import android.content.Intent;
+import android.media.AudioManager;
+import android.speech.RecognizerIntent;
+import android.speech.SpeechRecognizer;
 import android.speech.tts.TextToSpeech;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -8,7 +15,15 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.SeekBar;
+import android.widget.TextView;
+import android.widget.Toast;
 
+import com.github.zagum.speechrecognitionview.RecognitionProgressView;
+import com.github.zagum.speechrecognitionview.adapters.RecognitionListenerAdapter;
+import com.gun0912.tedpermission.PermissionListener;
+import com.gun0912.tedpermission.TedPermission;
+
+import java.util.ArrayList;
 import java.util.Locale;
 import java.util.Vector;
 
@@ -20,19 +35,41 @@ public class MainActivity extends AppCompatActivity implements TextToSpeech.OnIn
     SeekBar voice_speed,voice_pitch;
     float speak_speed = 1;
     float speak_pitch = 1;
+    String alltext = "";
+    SpeechRecognizer speechRecognizer;
+    RecognitionProgressView recognitionProgressView;
     openingLine opLine;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        speak_btn = (Button) findViewById(R.id.speak_btn);
-        voice_speed = (SeekBar)findViewById(R.id.voice_speed);
-        voice_pitch = (SeekBar)findViewById(R.id.voice_pitch);
-        input_text = (EditText)findViewById(R.id.editText2);
-        textToSpeech = new TextToSpeech(this,this);
-        speak_btn.setOnClickListener(this);
-        voice_seekBar_setting();
+        new TedPermission(this)
+                .setDeniedMessage("請賦予應用權限，否則無法正常使用\n\n請按下設定 [設定] > [權限] > 將權限打開")
+                .setPermissions(Manifest.permission.RECORD_AUDIO,
+                        Manifest.permission.INTERNET)
+                .setPermissionListener(new PermissionListener() {
+                    @Override
+                    public void onPermissionGranted() {
+                        speak_btn = (Button) findViewById(R.id.speak_btn);
+                        voice_speed = (SeekBar)findViewById(R.id.voice_speed);
+                        voice_pitch = (SeekBar)findViewById(R.id.voice_pitch);
+                        input_text = (EditText)findViewById(R.id.editText2);
+                        textToSpeech = new TextToSpeech(MainActivity.this,MainActivity.this);
+                        speak_btn.setOnClickListener(MainActivity.this);
+                        voice_seekBar_setting();
+                        setRecognition();
+                    }
+
+                    @Override
+                    public void onPermissionDenied(ArrayList<String> deniedPermissions) {
+                        Toast.makeText(MainActivity.this, "權限獲取失敗\n" + deniedPermissions.toString(), Toast.LENGTH_SHORT).show();
+                    }
+                })
+                .check();
+
+
     }
 
     @Override
@@ -79,13 +116,92 @@ public class MainActivity extends AppCompatActivity implements TextToSpeech.OnIn
         super.onDestroy();
     }
 
+    private void showResults(Bundle results) {
+        ArrayList<String> matches = results
+                .getStringArrayList(SpeechRecognizer.RESULTS_RECOGNITION);
+        String[] ssssss = matches.toArray(new String[0]);
+        //runSometing(ssssss);
+        //Log.d("MYLOG",ssssss[0]);
+        input_text.setText(ssssss[0]);
+        speakOut(ssssss[0]);
+        //Toast.makeText(this, str5+" / "+ Pinyin.toPinyin(str5,""), Toast.LENGTH_LONG).show();
+//        for (String s : ssssss) {
+//            alltext += s + " / " + Pinyin.toPinyin(s, "") + "\n";
+//        }
+        //voiceString.setText(alltext);
+        recognitionProgressView.stop();
+        recognitionProgressView.play();
+        //if(record==false)return;
+        //startRecognition();
+        recognitionProgressView.setVisibility(View.GONE);
+    }
+
+    private void setRecognition() {
+        int[] colors = {
+                ContextCompat.getColor(this, R.color.color1),
+                ContextCompat.getColor(this, R.color.color2),
+                ContextCompat.getColor(this, R.color.color3),
+                ContextCompat.getColor(this, R.color.color4),
+                ContextCompat.getColor(this, R.color.color5)
+        };
+
+        int[] heights = {60, 76, 58, 80, 55};
+
+
+        //voiceString = (TextView) findViewById(R.id.voiceString);
+        speechRecognizer = SpeechRecognizer.createSpeechRecognizer(this);
+        recognitionProgressView = (RecognitionProgressView) findViewById(R.id.recognition_view);
+        recognitionProgressView.setSpeechRecognizer(speechRecognizer);
+        recognitionProgressView.setRecognitionListener(new RecognitionListenerAdapter() {
+            @Override
+            public void onResults(Bundle results) {
+                Log.d("MYLOG", "onResults");
+                showResults(results);
+            }
+
+            @Override
+            public void onError(int error) {
+                super.onError(error);
+                //if(record==false)return;
+                //startRecognition();
+            }
+
+        });
+
+        recognitionProgressView.setColors(colors);
+        recognitionProgressView.setBarMaxHeightsInDp(heights);
+        recognitionProgressView.play();
+
+        AudioManager amanager = (AudioManager) getSystemService(Context.AUDIO_SERVICE);
+        amanager.setStreamMute(AudioManager.STREAM_NOTIFICATION, true);
+        amanager.setStreamMute(AudioManager.STREAM_ALARM, true);
+        amanager.setStreamMute(AudioManager.STREAM_MUSIC, true);
+        amanager.setStreamMute(AudioManager.STREAM_RING, true);
+        amanager.setStreamMute(AudioManager.STREAM_SYSTEM, true);
+    }
+
+    private void startRecognition() {
+        Intent intent = new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
+        intent.putExtra(RecognizerIntent.EXTRA_CALLING_PACKAGE, getPackageName());
+        intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL, RecognizerIntent.LANGUAGE_MODEL_FREE_FORM);
+        intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE, Locale.getDefault());
+        intent.putExtra(RecognizerIntent.EXTRA_MAX_RESULTS, 5);
+        speechRecognizer.startListening(intent);
+        Log.d("MYLOG", "Start Recording");
+    }
+
+
+
     @Override
     public void onClick(View v) {
         if(!textToSpeech.isSpeaking()) {
             if (v.getId() == R.id.speak_btn) {
 
-                speakOut(answer(input_text.getText().toString()));
+                //speakOut(answer(input_text.getText().toString()));
                 //Log.d("MYLOG",answer(input_text.getText().toString()));
+                startRecognition();
+                recognitionProgressView.setVisibility(View.VISIBLE);
+                speakOut("請說");
             }
         }else{
             speakOut("我還在講話你插什麼嘴我不想講了!");
